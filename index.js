9715754662dd2c7b4c09eca1fa3c89750746f405
Melissa Chen <myc2@illinois.edu>
2021-04-02 14:46:12 -0500

    scripting works :D

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-$('.collapse').collapse()
+"use strict";
+
+const mbti = {
+    "ENFJ": enfj,
+    "ENFP": enfp,
+    "ENTJ": entj,
+    "ENTP": entp,
+    "ESFJ": esfj,
+    "ESFP": esfp,
+    "ESTJ": estj,
+    "ESTP": estp,
+    "INFJ": infj,
+    "INFP": infp,
+    "INTJ": intj,
+    "INTP": intp,
+    "ISFJ": isfj,
+    "ISFP": isfp,
+    "ISTJ": istj,
+    "ISTP": istp,
+};
+
+Object.keys(mbti).forEach((key) => {
+    var inner = "";
+
+    Object.keys(mbti[key]).forEach((name_emoji) => {
+        inner += name_emoji + ", " + mbti[key][name_emoji] + "<br>";
+    });
+
+    document.getElementById(key).innerHTML = inner;
+});
